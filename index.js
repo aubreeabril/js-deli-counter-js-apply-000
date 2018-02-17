dfa7f7be1fa422e56a2a823191daba15016ebb6d
Aubree Abril <aubree.abril@gmail.com>
2018-02-17 22:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function currentLine(line) {
 
 
 // function for people entering deli. arguments = array for current line, new customer name. Return person's name and position in line.
+function newCustomer(deliLine, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
 
 
 // function to call next person and take them from front of line. If no one in line, return "There is nobody waiting to be served!"
