d5cbe1ca6eed192669c29a675798080df54b181d
Aubree Abril <aubree.abril@gmail.com>
2018-02-17 22:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function currentLine(line) {
 
     return `The line is currently: ${peopleInLine.join(', ')}`
   }
-}
+
 
 
 // function for people entering deli. arguments = array for current line, new customer name. Return person's name and position in line.
-function takeANumber(deliLine, name) {
-  deliLine.push(name);
+function takeANumber(line, name) {
+  line.push(name);
 
-  return `Welcome, ${name}. You are number ${peopleInLine.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 };
 
 // function to call next person and take them from front of line. If no one in line, return "There is nobody waiting to be served!"
@@ -27,6 +27,6 @@ function nowServing(line) {
   if(line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return  `Currently serving ${line.shift()}.`
+    return  `Currently serving ${line.shift()}.`;
   }
 };
