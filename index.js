e9651336942222014beae23e15d0e1a708ee048d
Aubree Abril <aubree.abril@gmail.com>
2018-02-17 22:47:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+// function to show people their current place in line or announce that the line is empty
+
 function currentLine(line){
   if(line.length === 0) {
     return "The line is currently empty.";
   }
   var peopleInLine = [];
-
+// accept new people in line and assign them to numbers
   for (var i = 0; i < line.length; i++) {
     peopleInLine.push(`${i + 1}. ${line[i]}`)
   }
+  // return the list of people in line
   return `The line is currently: ${peopleInLine.join(', ')}`
 };
-
+// Show the first person in line and remove them from the line.
 function nowServing(line) {
   if(line.length === 0) {
     return "There is nobody waiting to be served!"
@@ -17,7 +20,7 @@ function nowServing(line) {
     return `Currently serving ${line.shift()}.`
   };
 
-
+// welcome people to deli. Add new customers to the line.
 function takeANumber(line, name){
   line.push(name);
 
